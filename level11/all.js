c113860fb3be99f648411fe20916ad01203fd8a8
HARSHIL2023 <harshil.patel.cg@gmail.com>
2025-12-17 13:36:25 +0530

    added more question

--- level11/all.js
+++ level11/all.js
@@ -49,4 +49,78 @@ function betweeen1tp100(n){
 
  }
 
+//109
+
+function firstcharacter(str){
+    console.log(str[0]);
+
+}
+
+//110
+
+function lastcharacter(str){
+    console.log(str[str.length-1])
+}
+
+//111
+
+function longerstring(str1,str2){
+    str1>str2?console.log(str1):console.log(str2);
+}
+
+//112
+
+function average(a,b,c){
+    console.log((a+b+c)/3);
+}
+
+//113
+
+function positivenegative(num){
+    num>0?console.log("postive"):console.log("negative or zero")
+}
+
+//114
+
+function eligibletovote(age){
+    return age>18
+}
+
+//115
+
+function hourstominute(time){
+    console.log(60*time);
+}
+
+
+//116
+
+function minutetoSecond(time){
+    console.log(60*time);
+}
+
+//117
+
+function celsiustoFraheenhit(temp){
+    console.log((temp*9/5)+32);
+}
+
+//118
+
+function frenhittocelsius(temp){
+    console.log((temp-32)*5/9)
+}
+
+//119
+
+function areaoftrinangle(base,height){
+    console.log(0.5*base*height);
+}
+
+//120
+
+function areaofcircle(radius){
+    console.log(3.14*radius*radius);
+
+}
 
