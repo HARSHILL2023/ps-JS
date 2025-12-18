bb6dccb9b3b0968e5f224fd1ef9f1e788d161d47
HARSHIL2023 <harshil.patel.cg@gmail.com>
2025-12-18 10:17:22 +0530

    question done

---
+++ level13/all.js
@@ -0,0 +1,20 @@
+//141
+
+function middle(str){
+    let check=str.length;
+    if(check%2==0){
+        console.log(str[((check/2)-1)]+str[(check/2)])
+    }
+    else{
+        let temp2=Math.floor((check/2))
+        console.log(temp2);
+        
+        console.log(str[temp2])
+    }
+}
+
+//142
+
+function startWitha(str){
+    
+}
