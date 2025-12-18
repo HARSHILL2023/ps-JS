7f2c26213230ace55f51c4f4eb05a84f8ea5b5e7
HARSHIL2023 <harshil.patel.cg@gmail.com>
2025-12-18 20:10:31 +0530

    question done

--- level14/all.js
+++ level14/all.js
@@ -1,69 +1,229 @@
 //161
 
-function countall(arry){
-    let postive=0;
-    let negative=0;
-    let zero=0;
-    for(let i=0;i<arry.length;i++){
-        if(arry[i]==0){
+function countall(arry) {
+    let postive = 0;
+    let negative = 0;
+    let zero = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] == 0) {
             zero++;
         }
-        else if(arry[i]>0){
+        else if (arry[i] > 0) {
             postive++;
         }
-        else{
+        else {
             negative++;
         }
     }
     console.log(`postive ${postive} negative ${negative} zero ${zero}`);
-    
+
 
 }
 
 //162
 
-function triple(arry){
-    let triple=[];
-    for(let i=0;i<arry.length;i++){
-        triple[i]=arry[i]*3;
+function triple(arry) {
+    let triple = [];
+    for (let i = 0; i < arry.length; i++) {
+        triple[i] = arry[i] * 3;
     }
     console.log(triple);
-    
+
 }
 
 
 //163
 
-function absoulte(arry){
-    let neww=[];
-    for(let i=0;i<arry.length;i++){
-        neww[i]=Math.abs(arry[i]);
+function absoulte(arry) {
+    let neww = [];
+    for (let i = 0; i < arry.length; i++) {
+        neww[i] = Math.abs(arry[i]);
     }
     console.log(neww);
-    
+
 }
 //164
 
-function alleven(arry){
-     let count=0;
-     for(let i=0;i<arry.length;i++){
-        if(arry[i]%2==0){
+function alleven(arry) {
+    let count = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] % 2 == 0) {
             count++;
         }
-        
-     }
-     return arry.length==count;
+
+    }
+    return arry.length == count;
 }
 
 //165
 
-function atleatoneeven(arry){
-     let count=0;
-     for(let i=0;i<arry.length;i++){
-        if(arry[i]%2==0){
+function atleatoneeven(arry) {
+    let count = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] % 2 == 0) {
             count++;
         }
-        
-     }
-     return count>=1;
+
+    }
+    return count >= 1;
 }
+
+//166
+
+function allpostive(arry) {
+    let count = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] >= 0) {
+            count++;
+        }
+
+    }
+    return arry.length == count;
+
+}
+
+//167
+
+function elmentsgreatrerthank(k, arry) {
+    let count = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] > k) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+
+//168
+
+function indexofmaximum(arry) {
+    let max = arry[1];
+
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] >= max) {
+            max = arry[i];
+            print = i;
+        }
+    }
+    console.log(print);
+}
+
+
+//169
+
+function minumindex(arry){
+    let max = arry[1];
+
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] <= max) {
+            max = arry[i];
+            print = i;
+        }
+    }
+    console.log(print);
+}
+
+//170
+function duplicate(arry) {
+    for (let i = 0; i < arry.length; i++) {
+        for (let j = i + 1; j < arry.length; j++) {
+            if (arry[i] === arry[j]) {
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+
+//171
+
+function uniqueElements(arr){
+    let arrNew = [];
+    let obj = {};
+    for(let i = 0 ; i<arr.length ; i++){
+        if(obj[arr[i]]){
+            continue;
+        }else {
+            obj[arr[i]] = 1;
+            arrNew.push(arr[i]);
+        }
+    }
+    return arrNew;
+}
+
+//172
+
+function increasigarray(arry){
+    let count=0;
+    for(let i=0;i<arry.length;i++){
+        if(arry[i]<arry[i+1]){
+            count++;
+        }
+    }
+    return count==arry.length-1;
+}
+
+//173
+
+
+
+//174
+
+function maximumdiffereence(arry){
+    let max=arry[0];
+    let min=arry[0];
+
+    for(let i=0;i<arry.length;i++){
+        if(arry[i]>max){
+            max=arry[i]
+        }
+        if(arry[i]<min){
+            min=arry[i];
+        }
+    }
+    let diff=max-min;
+    console.log(diff);
+    
+}
+
+//175
+
+function evenindices(arry){
+    let even=[];
+    for(let i=0;i<arry.length;i++){
+        if(i%2==0){
+            even.push(arry[i]);
+        }
+
+    }
+    console.log(even);
+    
+}
+
+//176
+
+function oddindeces(arry){
+      let odd=[];
+    for(let i=0;i<arry.length;i++){
+        if(i%2!=0){
+            odd.push(arry[i]);
+        }
+
+    }
+    console.log(odd);
+}
+
+//178
+
+function firstindex(arry){
+    let neww=[]
+    for(let i=0;i<arry.length;i++){
+        neww.push(arry[i][0])
+    }
+    console.log(neww);
+    
+}
+
