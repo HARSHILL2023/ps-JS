3533c7d4ba72167d13e2e272a32373ae5778ce58
HARSHIL2023 <harshil.patel.cg@gmail.com>
2025-12-29 11:06:16 +0530

    chnage

--- level15/all.js
+++ level15/all.js
@@ -58,3 +58,52 @@ function rotatearryy(arry, k) {
     }
     console.log(arry);
 }
+
+
+//Q-185
+
+function arr(arr1, arr2) {
+  let i=0,j=0;
+  let res=[];
+
+  while(i<arr1.length && j<arr2.length){
+    if(arr1[i]<arr2[j]){
+      res.push(arr1[i]);
+      i++;
+    }
+    else{
+      res.push(arr2[j]);
+      j++;
+    }
+  }
+  while(i<arr1.length){
+    res.push(arr1[i]);
+    i++;
+  }
+  while(j<arr2.length){
+    res.push(arr2[j]);
+    j++;
+  }
+  console.log(res);
+}
+
+arr([1,3,5],[2,4,6]);
+
+
+//Q-186
+function bubble(arr){
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length-i;j++){
+      let temp=arr[j];
+      if(arr[j]>arr[j+1]){
+        arr[j] = arr[j+1];
+         arr[j+1] = temp;
+         
+       
+      }
+    }
+  }
+  console.log(arr);
+}
+
+//187
